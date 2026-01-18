cb688780dab22ae3676adc8608cc7b56bcefa072
prathmesh <iprathmeshsangam@gmail.com>
2026-01-18 20:47:44 +0530

    nested loops. java

---
+++ Lessons/src/nestedLoops.java
@@ -0,0 +1,15 @@
+public class nestedLoops {
+
+    public static void main(String[] args)
+    {
+        //Nested Loop is basically Loop inside another loop
+
+
+        for (int i = 1 ; i <= 3 ; i ++){
+            for (int j = 1 ; j < 10 ; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
